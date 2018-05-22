c3d15c0eb7884944fca31883cc0ff57b2b19c322
Votre nom ou pseudo <Votre@email.com>
2018-05-22 09:47:42 +0200

    Hello World

---
+++ cmake-build-debug/CMakeFiles/opencv_basic_editor.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opencv_basic_editor.dir/main.cpp.o"
+  "opencv_basic_editor.pdb"
+  "opencv_basic_editor"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opencv_basic_editor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
